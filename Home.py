31fa54f5b2b24cb482fe6e4cf2e6ca691f7848dd
Alkin Valeta <94931387+alkymvaletta@users.noreply.github.com>
2024-07-01 14:56:26 -0500

    se agregan avance hasta la fecha

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+HORIZONTAL = 'src\images\horizontal_logo.jpeg'
+
+st.logo(HORIZONTAL)
+
+st.set_page_config(page_title='Inicio', page_icon='🐔', layout='centered')
+
+st.header('Avicudatos 🐔', divider='rainbow')
+st.subheader('Analizamos el rendimiento de tus aves')
+
+
+
+
+
+st.write('Aquí inicia mi tesis')
